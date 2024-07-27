b74db08d66a295b7897465644a8835e5e8a2be36
--global <metinkale38@gmail.com>
2024-07-27 21:57:50 +0200

    cors header

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -26,10 +26,27 @@ dependencies {
     implementation("io.ktor:ktor-server-host-common-jvm")
     implementation("com.ucasoft.ktor:ktor-simple-cache-jvm:0.4.3")
     implementation("com.ucasoft.ktor:ktor-simple-memory-cache-jvm:0.4.3")
+    implementation("io.ktor:ktor-server-cors")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+
+
+tasks.named("compileJava") {
+    dependsOn(":webapp:npmBuild")
+}
+
+tasks.register<Copy>("copyWebApp") {
+    dependsOn(":webapp:npmBuild")
+    from("$rootDir/webapp/build")
+    into("$buildDir/resources/main/static/.")
+}
+
+tasks.named("processResources") {
+    dependsOn("copyWebApp")
 }
--- server/src/main/kotlin/dev/metinkale/prayertimes/Application.kt
+++ server/src/main/kotlin/dev/metinkale/prayertimes/Application.kt
@@ -10,6 +10,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.minutes
@@ -27,6 +28,9 @@ fun Application.module() {
             encodeDefaults = true
             explicitNulls = false
         })
+    }
+    install(CORS) {
+        allowHost("localhost:3000")
     }
     install(SimpleCache) {
         memoryCache {
@@ -48,5 +52,6 @@ fun Application.module() {
     }
     routing {
         route("/api", Route::api)
+        staticResources("/", "static")
     }
 }
